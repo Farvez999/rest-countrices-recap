e274784172d47085b99cd6bc6300761d83bf3a5a
Farvez Abdullah <engrfrzabdullah999@gmail.com>
2022-09-18 23:14:02 +0600

    rest-countrices-name & flags load

---
+++ js/countrices.js
@@ -0,0 +1,45 @@
+const loadCountrices = () => {
+    fetch('https://restcountries.com/v3.1/all')
+        .then(res => res.json())
+        .then(data => displayCountries(data));
+}
+
+displayCountries = countries => {
+    console.log(countries)
+    const countryHTML = countries.map(country => getCountryHTML(country))
+    console.log(countryHTML)
+    const container = document.getElementById('countries');
+    container.innerHTML = countryHTML.join(' ')
+}
+
+getCountryHTML = (name, flags) => {
+    return `
+        <div class ="country">
+        <h2>${name.common}</h2>
+        <img src="${flags.png}">
+        </div>
+        `
+}
+
+// option 1
+// getCountryHTML = country => {
+//     const { name, flags } = country;
+//     return `
+//     <div class ="country">
+//     <h2>${name.common}</h2>
+//     <img src="${flags.png}">
+//     </div>
+//     `
+// }
+
+// orginal 
+// getCountryHTML = country => {
+//     return `
+//     <div class ="country">
+//     <h2>${country.name.common}</h2>
+//     <img src="${country.flags.png}">
+//     </div>
+//     `
+// }
+
+loadCountrices();
